cb70840580ec7cb161253d836e72512f25af4670
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-12-02 11:09:53 +0100

    eliminated dist

--- dist/contacto.8b5c4bd2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("liveToastBtn").addEventListener("click", function() {
-    const toastEl = document.querySelector(".toast");
-    const toast = new bootstrap.Toast(toastEl);
-    toast.show();
-});
-
-//# sourceMappingURL=contacto.8b5c4bd2.js.map
